812d529c5c8e125bc3dcd0890b8cad3228b371c6
fuerbringer <severin@protonmail.ch>
2017-08-26 16:05:02 +0200

    Implemented overlay.

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,6 +23,19 @@ module.exports = function (grunt) {
         mangle: false
       }
     },
+    less: {
+      development: {
+        options: {
+          compress: true,
+          yuicompress: true,
+          optimization: 2
+        },
+        files: {
+          "public/stylesheets/css/main.css": "public/stylesheets/less/main.less",
+          "public/stylesheets/css/overlay.css": "public/stylesheets/less/overlay.less"
+        }
+      }
+    },
     cssmin: {
       options: {
         mergeIntoShorthands: false,
@@ -32,7 +45,20 @@ module.exports = function (grunt) {
         files: {
           'public/stylesheets/thirdparty/thirdparty.min.css': [
             'node_modules/bootstrap/dist/css/bootstrap.min.css',
+          ],
+          'public/stylesheets/minified/master.min.css': [
+            'public/stylesheets/css/overlay.css',
+            'public/stylesheets/css/main.css',
           ]
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['public/stylesheets/less/*.less'],
+        tasks: ['less', 'cssmin'],
+        options: {
+          nospawn: true
         }
       }
     }
@@ -40,5 +66,7 @@ module.exports = function (grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-uglify')
   grunt.loadNpmTasks('grunt-contrib-cssmin')
-  grunt.registerTask('default', ['uglify', 'cssmin'])
+  grunt.loadNpmTasks('grunt-contrib-less')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.registerTask('default', ['uglify', 'less', 'cssmin', 'watch'])
 }
