17c212b142dc7fe85f069ae3400a2171eb3cf4f0
fuerbringer <severin@protonmail.ch>
2018-04-02 21:53:38 +0200

    Implement sweetalert localstorage strapi authentication.

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const Config = require('../models/config')
-const Columns = require('../models/columns')
 
 router.get('/', function (req, res, next) {
-  var cfg = new Config()
-  var cols = new Columns()
-
-  var cfgData = cfg.getAll()
-  var rows = cols.getRows(12 / cfg.getColumnWidth())
-
-  res.render('index', {
-    cfgData, rows
-  })
+  res.render('index')
 })
 
 module.exports = router
