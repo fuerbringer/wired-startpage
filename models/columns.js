27306d40190ebc430fed9d3508ed0a88b4b85e32
fuerbringer <severin@protonmail.ch>
2018-05-21 19:46:15 +0200

    Implement yaml for tile/columns.

--- models/columns.js
+++ models/columns.js
@@ -1,16 +1,26 @@
-const jsonfile = require('jsonfile')
+const fs = require('fs')
+const yaml = require('js-yaml')
 
-Columns.DEFAULT_FILE = 'data/columns.json'
-Columns.EXAMPLE_FILE = Columns.DEFAULT_FILE + '.example'
+Columns.DEFAULT_TILES = 'data/tiles/'
+Columns.EXAMPLE_TILE = 'data/tile.example.yaml'
 
 function Columns (path = Columns.DEFAULT_FILE) {
-  try {
-    this.data = jsonfile.readFileSync(path)
-  } catch (error) {
-    this.data = jsonfile.readFileSync(Columns.EXAMPLE_FILE)
-    if (error.code === 'ENOENT') {
-      console.log('`' + Columns.DEFAULT_FILE + '` not found. Falling back to `' + Columns.EXAMPLE_FILE + '`.')
+  this.data = {
+    columns: []
+  }
+  const tilesDirectory = fs.readdirSync(Columns.DEFAULT_TILES)
+  const yamlTiles = tilesDirectory.filter(file => { return file !== '.gitignore' })
+  if (yamlTiles.length) {
+    // Custom tiles present
+    for (var i = 0; i < yamlTiles.length; i++) {
+      const tempTile = yaml.safeLoad(fs.readFileSync(Columns.DEFAULT_TILES + yamlTiles[i], 'utf-8'))
+      this.data.columns.push(tempTile)
     }
+  } else {
+    // Use default tile
+    console.log('No user-defined tiles found. Falling back to `' + Columns.EXAMPLE_TILE + '`.')
+    const tempTile = yaml.safeLoad(fs.readFileSync(Columns.EXAMPLE_TILE, 'utf-8'))
+    this.data.columns.push(tempTile)
   }
 }
 
